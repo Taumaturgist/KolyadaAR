ad7f33335d8db24f465277a80f5cdaf8b9c37518
grigoryPischaev <pischaev@sputnikfund.ru>
2023-12-20 20:33:13 +0700

    [Character] Remove old list

--- Project/Assets/ARCarols/Scripts/Character/Config/CharacterTextConfig.cs
+++ Project/Assets/ARCarols/Scripts/Character/Config/CharacterTextConfig.cs
@@ -7,8 +7,6 @@ namespace ARCarols.Scripts.Character.Config
     [CreateAssetMenu(menuName = "Project/Character/CharacterTextConfig", fileName = "Character Text Config", order = 0)]
     public class CharacterTextConfig : ScriptableObject
     {
-        [SerializeField] private List<string> _textList;
-
         [SerializeField] private List<CharacterTextProvider> _characterTextProviders;
 
         public List<CharacterTextProvider> CharacterTextProviders => _characterTextProviders;
